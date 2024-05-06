b592a8b50ff1e762b25f72e8bc27e8d6e67fd6c5
alexsh882 <alexsh882@gmail.com>
2024-05-06 14:21:29 -0300

    Se agregó mejoras a la vista detalle del personaje.

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,4 +1,4 @@
-import { View  } from "react-native";
+import { View } from "react-native";
 import { useContext, useEffect, useState } from "react";
 import { CharactersContext } from "../contexts/CharactersContext";
 import { ScaledSheet } from "react-native-size-matters";
@@ -12,7 +12,13 @@ export default function DetailsScreen({ route }) {
     const [character, setCharacter] = useState({
         name: '',
         status: '',
+        type: '',
         image: '',
+        species: '',
+        gender: '',
+        origin: {
+            name: ''
+        }
 
     })
 
@@ -21,14 +27,33 @@ export default function DetailsScreen({ route }) {
     useEffect(() => {
         const character = showCharacter(id);
         setCharacter(character);
-    }, []);
+    }, [id]);
 
 
     return (
         <View style={styles.container}>
             <Avatar.Image size={styles.imageSize} source={{ uri: character.image }} />
-            <Text style={styles.fontSizeTitle}>{character?.name}</Text>
-            <Text>{character?.status}</Text>
+            <Text style={styles.fontSizeTitle}>{character?.species}</Text>
+            <View style={{
+                display: 'flex',
+                flexDirection: 'row',
+                gap: 10
+            }}>
+                <Text style={styles.fontSizeTitle}>{character?.name}</Text>
+                <Text style={
+                    {
+                        color: character?.status === 'Alive'
+                            ? 'green' : character?.status === 'Dead' ? 'red' : 'gray'
+                    }
+                }>{
+
+                        character?.status === 'Vivo'
+                            ? 'green' : character?.status === 'Dead' ? 'Muerto' : 'Desconocido'}</Text>
+
+            </View>
+            <Text>{character?.gender == "Male" ? 'Hombre' : character?.gender == "Female" ? 'Mujer' : "Desconocido"}</Text>
+            <Text>{character.type}</Text>
+            <Text>{'Origen: ' + character?.origin?.name}</Text>
         </View>
     )
 }
